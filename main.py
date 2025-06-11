d41b7fcab3a3c91a6114c95a2d10eef8d046d0b0
yvonne whiting <yvonnewhiting@hotmail.co.uk>
2025-06-11 21:27:18 +0100

    added move logic and tidied up code with groups

--- main.py
+++ main.py
@@ -5,19 +5,27 @@ from player import Player
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while pygame.init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-            player.update(dt)
+            updateable.update(dt)
 
             screen.fill("black")
-            player.draw(screen)
+
+            for item in drawable:
+                item.draw(screen)
+        
             pygame.display.flip()
 
             dt = clock.tick(60) / 1000
