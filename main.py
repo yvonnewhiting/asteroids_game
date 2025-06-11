04f461a73773701f0594019950175db91cf58c6c
yvonne whiting <yvonnewhiting@hotmail.co.uk>
2025-06-11 13:43:58 +0100

    added player triangle logic and turn left/right logic

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while pygame.init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+            player.update(dt)
+
             screen.fill("black")
+            player.draw(screen)
+            pygame.display.flip()
+
+            dt = clock.tick(60) / 1000
     
     
 
