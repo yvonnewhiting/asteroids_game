dfb91c2fadf4064988df721b1fbf53ef3fe0535e
yvonne whiting <yvonnewhiting@hotmail.co.uk>
2025-06-11 22:13:35 +0100

    sdded asteroid logic and tidied up groups

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = updateable
+    asteroid_field = AsteroidField()
+
     Player.containers = (updateable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
 
-    while pygame.init():
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-            updateable.update(dt)
+        updateable.update(dt)
 
-            screen.fill("black")
+        screen.fill("black")
 
-            for item in drawable:
-                item.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         
-            pygame.display.flip()
+        pygame.display.flip()
 
-            dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
     
     
 
