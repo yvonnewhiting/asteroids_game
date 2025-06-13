8d5ea375fdd2ae149fa353b708b589272672acde
yvonne whiting <yvonnewhiting@hotmail.co.uk>
2025-06-13 09:36:41 +0100

    added collsion logic of asteroids

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -29,6 +30,11 @@ def main():
                 return
 
         updateable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
